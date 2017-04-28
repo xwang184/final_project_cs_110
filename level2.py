476a555075fd1a567497027c62eabc4931f030b9
jessicayin20 <jyin11@binghamton.edu>
2017-04-28 16:35:15 -0400

    Update and rename main.py to level2.py

--- level2.py
+++ level2.py
@@ -56,48 +56,48 @@ while gameLoop:
                 index += 1
                 if index == image_length:
                     index = 0
-                
-                
+        
+        
             if event.key == pygame.K_LEFT:
                 index -= 1
                 if index == -1:
                     index = image_length - 1
-                
-            window.blit(image[index],(0,0))
-            window.blit(title, (0, 0))
-            window.blit(image_info, (0, 30))
+
+window.blit(image[index],(0,0))
+    window.blit(title, (0, 0))
+        window.blit(image_info, (0, 30))
             if remote_found:
                 window.blit(remote_image, (0, 500))
-            
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x1, y1 = pygame.mouse.get_pos()
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        x1, y1 = pygame.mouse.get_pos()
             if index == 3:
                 if x1>= 568 and x1 <= 596 and y1 >= 395 and y1 <= 399:
                     remote_found = True
                 if x1>= 264 and x1 <= 344 and y1 >= 230 and y1 <= 308:
                     print("key got")
                     key.key_got()
-                    
-            if remote_found and x1>=42 and x1 <= 50 and y1>=522 and y1 <= 533:
-                remote.click()
+
+        if remote_found and x1>=42 and x1 <= 50 and y1>=522 and y1 <= 533:
+            remote.click()
                 laptop.mainloop()
             if remote_found and x1>=64 and x1 <= 74 and y1>=541 and y1 <= 553:
                 remote.close()
                 laptop.mainloop()
-                
-            if index == 1:
-                if x1>= 274 and x1<= 526 and y1>= 150 and y1<=297:
-                    print("key got")
-                    key.key_got()
 
+if index == 1:
+    if x1>= 274 and x1<= 526 and y1>= 150 and y1<=297:
+        print("key got")
+            key.key_got()
+            
             if index == 0:
                 if x1>= 191 and x1<= 213 and y1>= 325 and y1<=337:
                     if key.finish():
                         gameLoop = False
                     else:
                         print("You must collect all the images to open the door!")
-            
-            key_number = str(key.sum) + "/3 collected."
+    
+        key_number = str(key.sum) + "/3 collected."
             image_info = font_title.render(key_number, 1, (0,0,0))
             window.blit(image[index],(0,0))
             window.blit(title, (0, 0))
@@ -107,8 +107,7 @@ while gameLoop:
                 
             
             
-    
-    pygame.display.flip()
+pygame.display.flip()
 
 
 
