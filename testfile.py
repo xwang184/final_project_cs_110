4df16913eb3f67683095d2004bccd8d626934090
xwang184 <xwang184@binghamton.edu>
2017-04-28 23:21:13 -0400

    Add files via upload

---
+++ testfile.py
@@ -0,0 +1,14 @@
+import unittest
+import pygame
+from jy import laptop
+
+def main():
+    a = 'scar'
+    b = input("Please enter your answer")
+    if a == b:
+        print("your answer is correct")
+    else:
+        print("Please try again")
+
+
+main()
